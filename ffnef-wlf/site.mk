5629d750a4a308ce436388f5d102a7720200745a
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2016-01-10 00:29:04 +0100

    Added site.conf files for cities in Kreis Mettmann

---
+++ ffnef-wlf/site.mk
@@ -0,0 +1,40 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-announced \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	haveged \
+	iptables \
+	iwinfo \
+	ffho-ebtables-net-rules \
+	ffho-txpower-fix \
+	ffho-wifi-blackout-workaround \
+	ffho-autoupdater-wifi-fallback \
+	gluon-ebtables-filter-roguenets  	
+
+	
+
+DEFAULT_GLUON_RELEASE := $(shell date '+%Y%m%d%H%M')-exp
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en fr
