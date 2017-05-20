95d272af5e2f496371b050f56a10dfeec89998e2
Benedikt Wildenhain <esperanto@benedikt-wildenhain.de>
2017-05-20 17:03:30 +0200

    community LVR added

---
+++ ffnef-lvr/site.mk
@@ -0,0 +1,38 @@
+GLUON_SITE_PACKAGES := \
+	gluon-mesh-batman-adv-15 \
+	gluon-alfred \
+	gluon-respondd \
+	gluon-autoupdater \
+	gluon-config-mode-autoupdater \
+	gluon-config-mode-contact-info \
+	gluon-config-mode-core \
+	gluon-config-mode-geo-location \
+	gluon-config-mode-hostname \
+	gluon-config-mode-mesh-vpn \
+	gluon-ebtables-filter-multicast \
+	gluon-ebtables-filter-ra-dhcp \
+	gluon-luci-admin \
+	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
+	gluon-luci-private-wifi \
+	gluon-luci-wifi-config \
+	gluon-next-node \
+	gluon-mesh-vpn-fastd \
+	gluon-radvd \
+	gluon-setup-mode \
+	gluon-status-page \
+	gluon-txpowerfix \
+	haveged \
+	iptables \
+	iwinfo \
+	gluon-ebtables-filter-roguenets  	
+
+	
+
+DEFAULT_GLUON_RELEASE := $(shell date '+v2016.2.4-%m%d')
+
+# Allow overriding the release number from the command line
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+GLUON_LANGS ?= de en fr
